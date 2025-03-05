b5468a62b281f1e1f8cbd6b69be17d8cf1762350
LuqmanRM <luqmanrofif2018@mail.ugm.ac.id>
2025-03-05 09:08:55 +0700

    init swagger

--- apps/app.go
+++ apps/app.go
@@ -3,6 +3,7 @@ package apps
 import (
 	"crud_fiber.com/m/config"
 	"crud_fiber.com/m/config/database"
+	_ "crud_fiber.com/m/docs"
 	"crud_fiber.com/m/handler"
 	"crud_fiber.com/m/middleware"
 	"crud_fiber.com/m/repository"
@@ -10,10 +11,16 @@ import (
 	"crud_fiber.com/m/service"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	fiberSwagger "github.com/swaggo/fiber-swagger"
 	"gorm.io/gorm"
 	"log"
 )
 
+// @title CRUD Fiber API
+// @version 1.0
+// @description Dokumentasi API CRUD menggunakan go fiber
+// @host localhost:8080
+// @BasePath /api/v1
 func StartApps() {
 	app := fiber.New(fiber.Config{
 		BodyLimit: 100 * 1024 * 1024,
@@ -51,6 +58,8 @@ func StartApps() {
 	}
 
 	authRoute.SetupAuthRoute()
+
+	app.Get("/swagger/*", fiberSwagger.WrapHandler)
 
 	errApp := app.Listen(":8080")
 	if errApp != nil {
